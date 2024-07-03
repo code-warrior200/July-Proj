a95559b616dfa93167528ff5ed3a699913a400c4
Code~warrior200 <61137642+code-warrior200@users.noreply.github.com>
2024-07-03 06:04:43 -0700

    git commit -m

--- qonect_app/lib/main.dart
+++ qonect_app/lib/main.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: use_key_in_widget_constructors
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'providers/auth_provider.dart';
@@ -11,7 +12,9 @@ import 'screens/sign_up_screen.dart';
 import 'screens/profile_screen.dart';
 import 'screens/chat_list_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
 
